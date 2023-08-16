171791fce516ef8ec98bb6b2e31624cbf0567295
zhangkuian <1352368240@qq.com>
2023-08-16 22:06:26 +0800

    feat: add publish subscribe mode for dynamic updates

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+let watcher = null
+
 function Vue(option) {
   const data = this._data = option.data
   observe(data)
@@ -17,16 +19,19 @@ function Vue(option) {
 
 // binding responsive
 function observe(data) {
+  const dep = new Dep()
   for (let key in data) {
     let val = data[key]
     if (typeof val === 'object') observe(val)
     Object.defineProperty(data, key, {
       get() {
+        watcher && dep.addSub(watcher)
         return val
       },
       set(newVal) {
         val = newVal
         if (typeof val === 'object') observe(val)
+        dep.notify()
       }
     })
   }
@@ -51,6 +56,9 @@ function replace(fragment, vm) {
     if (node.nodeType === 3 && reg.test(text)) {
       const val = getValue(vm, RegExp.$1)
       node.textContent = text.replace(reg, val)
+      new Watcher(vm, RegExp.$1, function (newVal) {
+        node.textContent = text.replace(reg, newVal)
+      })
     }
     // if nodeType === 1, it is element
     if (node.nodeType === 1) {
@@ -63,12 +71,42 @@ function replace(fragment, vm) {
         node.addEventListener('input', function (e) {
           setValue(vm, exp, e.target.value)
         })
+        new Watcher(vm, exp, function (newVal) {
+          node.value = newVal
+        })
       })
     }
     if (node.childNodes) {
       replace(node, vm)
     }
   })
+}
+
+//
+function Dep() {
+  this.subs = []
+}
+
+Dep.prototype.addSub = function (sub) {
+  this.subs.push(sub)
+}
+
+Dep.prototype.notify = function () {
+  this.subs.forEach(sub => sub.update())
+}
+
+function Watcher(vm, exp, fn) {
+  this.vm = vm
+  this.exp = exp
+  this.fn = fn
+  watcher = this
+  // access key trigger get within Object.defineProperty place watcher in subs
+  getValue(vm, exp)
+  watcher = null
+}
+
+Watcher.prototype.update = function () {
+  this.fn(getValue(this.vm, this.exp))
 }
 
 // accessing getValue a to trigger get within Object.defineProperty
